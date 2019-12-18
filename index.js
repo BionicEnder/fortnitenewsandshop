fa7a10d924e42e3f0bc92ea9252d26f73497ce48
BionicEnder <59005842+BionicEnder@users.noreply.github.com>
2019-12-17 23:19:55 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-document.getElementById('date').innerHTML = new Date().toDateString();toTimeString
+document.getElementById('date').innerHTML = new Date().toDateString();
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/Fortnite-API/js-wrapper@0.2.0/dist/fortnite-api.min.js"></script>
+var shop = fortniteApi.shop
+	.then(res => res.json())
+		document.getElementById(`yes`).ineerHTML = res.data.daily[0].items[0].name
+		document.getElementById(`shop`).innerHTML = res.data.daily[0].items[0].images.icon.urltoTimeString
