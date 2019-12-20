2b8db3177c835def0cd7d9b3b040aa08a35c1308
BionicEnder <59005842+BionicEnder@users.noreply.github.com>
2019-12-19 21:46:30 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("date").innerHTML = new Date().toDateString();
+document.getElementById("date").innerHTML = new Date().toTimeString();
 var api = require(fortnite)
 var config = {
 	apikey:"6a4f122f5fe17523716030bfa8ba655bb4e60db5a7826f3be868a0f5248a6990",
